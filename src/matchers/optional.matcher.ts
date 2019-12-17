8498726257e7b745af32690e51a30732818947bb
Julian Lang <julian.lang@hotmail.de>
2019-12-17 18:53:51 +0100

    fix: optional should throw if called with argument

--- src/matchers/optional.matcher.ts
+++ src/matchers/optional.matcher.ts
@@ -1,10 +1,13 @@
-import { createMatcher, isDefined } from '../util';
+import { ArgumentValueSymbol } from '../types';
+import { createMatcher, hasSymbol, isDefined } from '../util';
 
 export const optional = createMatcher('OptionalMatcher', optionalMatcher);
 
 function optionalMatcher(arg: any) {
-  if (arg === undefined) {
-    throw new Error(`optional-Matcher expects another matcher as argument: optional(MatcherFn)`);
+  if (arg === undefined || hasSymbol(ArgumentValueSymbol, arg)) {
+    throw new Error(
+      `optional-Matcher expects another matcher as argument: optional(MatcherFn | value)`,
+    );
   }
 
   return createMatcher('OptionalMatcher', (a: any) => arg(a) || !isDefined(a));
