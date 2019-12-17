48dee03554b7509fa779eb304390a410dddb2d38
Julian Lang <julian.lang@hotmail.de>
2019-12-17 21:56:27 +0100

    feat: add spec helper fns

--- specs/spec-helpers/index.ts
+++ specs/spec-helpers/index.ts
@@ -1,5 +1,5 @@
-import { ArgMatcher, ArgumentValueSymbol } from '../../src';
-import { createMatcher } from '../../src/util';
+import { ArgMatcher, ArgMatcherSymbol, ArgumentValueSymbol } from '../../src';
+import { createMatcher, isDefined, setSymbol } from '../../src/util';
 
 export const is42Fn = (a: number) => a === 42;
 export const is42Matcher = createMatcher('Is42', is42Fn);
@@ -9,4 +9,16 @@ export function testThrowForArgumentCall(matcher: ArgMatcher) {
     // arrange, act, assert
     expect(() => matcher({ [ArgumentValueSymbol]: true })).toThrow();
   });
+}
+
+export function spy(name?: string, fn?: jasmine.Func): jasmine.Spy {
+  return jasmine.createSpy(name, fn);
+}
+
+export function spyMatcher(returnValue?: boolean, name?: string, fn?: jasmine.Func): jasmine.Spy {
+  const spyInstance = isDefined(returnValue)
+    ? spy(name, fn).and.returnValue(returnValue)
+    : spy(name, fn);
+
+  return setSymbol(ArgMatcherSymbol, spyInstance);
 }
