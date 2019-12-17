2b891b3244569d22a7e0d1c7c9e6b4afef9ef69e
Julian Lang <julian.lang@hotmail.de>
2019-12-17 16:32:14 +0100

    fix: only change undefined to true

--- src/util/set-symbol.ts
+++ src/util/set-symbol.ts
@@ -1,7 +1,5 @@
-import { isDefined } from './is-defined';
-
 export function setSymbol<T extends symbol, K, V>(symbol: T, on: K, value?: V): K & Record<T, V> {
-  if (!isDefined(value)) {
+  if (value === undefined) {
     value = true as any;
   }
 
