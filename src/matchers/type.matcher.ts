ad76eb6436882e7e6b15fb17445d96bf288fbdec
Julian Lang <julian.lang@hotmail.de>
2019-12-17 18:19:44 +0100

    refactor: precise type for arg of type matcher fn

--- src/matchers/type.matcher.ts
+++ src/matchers/type.matcher.ts
@@ -1,10 +1,10 @@
 import { isPrimitive } from 'util';
-import { ArgMatcher, ArgumentValueSymbol } from '../types';
+import { ArgMatcher, ArgumentValueSymbol, TypeOfString } from '../types';
 import { createMatcher, hasSymbol, isDefined } from '../util';
 
 export const type = createMatcher('TypeMatcher', typeMatcher);
 
-function typeMatcher(arg: any): ArgMatcher {
+function typeMatcher(arg: TypeOfString): ArgMatcher {
   if (!isDefined(arg) || !isPrimitive(arg) || hasSymbol(ArgumentValueSymbol, arg)) {
     throw new Error(`type-Matcher expects a type string as argument: type('function')`);
   }
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,4 @@ export * from './arg-matcher';
 export * from './func';
 export * from './match.rule';
 export * from './symbols';
+export * from './typeof.string';
