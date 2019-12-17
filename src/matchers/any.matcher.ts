9d3fb3772e8103a7afecad6d2be9fe565b4885bf
Julian Lang <julian.lang@hotmail.de>
2019-12-17 17:10:15 +0100

    refactor: remove argument from any matcher

--- src/matchers/any.matcher.ts
+++ src/matchers/any.matcher.ts
@@ -1,12 +1,8 @@
 import { ArgMatcher, ArgMatcherSymbol } from '../types';
-import { createMatcher, isDefined, setSymbol } from '../util';
+import { createMatcher, setSymbol } from '../util';
 
 export const any = setSymbol(ArgMatcherSymbol, anyMatcher);
 
-function anyMatcher(arg: any): ArgMatcher {
-  if (isDefined(arg)) {
-    throw new Error(`any-Matcher does not expect any arguments. Use it like this: "[any, ...]".`);
-  }
-
+function anyMatcher(): ArgMatcher {
   return createMatcher('AnyMatcher', () => true);
 }
