4fb6dcee083087960b9a72ddea218e11b0e9f0f9
Julian Lang <julian.lang@hotmail.de>
2019-12-17 19:04:53 +0100

    fix: throw for obvious direct usage of unset

--- src/matchers/unset.matcher.ts
+++ src/matchers/unset.matcher.ts
@@ -1,7 +1,13 @@
-import { createMatcher, isDefined } from '../util';
+import { isPrimitive } from 'util';
+import { ArgumentValueSymbol } from '../types';
+import { createMatcher, hasSymbol, isDefined } from '../util';
 
 export const unset = createMatcher('UnsetMatcher', unsetMatcher);
 
 function unsetMatcher(arg: any) {
+  if (!isPrimitive(arg) && !hasSymbol(ArgumentValueSymbol, arg)) {
+    throw new Error(`unset-Matcher expects no arguments. Use it like that: "[unset, ...]"`);
+  }
+
   return !isDefined(arg);
 }
