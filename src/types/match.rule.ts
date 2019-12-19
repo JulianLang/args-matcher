e6d6a9d956f3a34f264c11fd174f127e09c6c9ea
Julian Lang <julian.lang@hotmail.de>
2019-12-19 22:35:35 +0100

    refactor: types, distinguish between set-rules and and-rules

--- src/types/match.rule.ts
+++ src/types/match.rule.ts
@@ -1,9 +1,27 @@
+import { AnyObject } from './any-object';
 import { ArgMatcher } from './arg-matcher';
 import { SetterFn, SetterFnWrapper } from './setter.fn';
 
 export type MatchExpr = ArgMatcher | any;
-export interface MatchRule {
+
+export type MatchRule = SetMatchRule | AndMatchRule;
+
+interface MatchRuleBase {
   name?: string;
   when: MatchExpr[] | MatchExpr;
+}
+
+export interface SetMatchRule extends MatchRuleBase {
+  set: SetterFnWrapper<any[]> | SetterFn | any;
+  and?: never;
+}
+
+export interface AndMatchRule extends MatchRuleBase {
+  and: SubMatchRule[];
+  set?: never;
+}
+
+export interface SubMatchRule {
+  args: AnyObject<MatchExpr[] | MatchExpr>;
   set: SetterFnWrapper<any[]> | SetterFn | any;
 }
